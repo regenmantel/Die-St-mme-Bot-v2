3ee433922fea4d6cfbe6b41bfcc0b036c9195c73
johnb <johnbernhard@live.de>
2024-02-17 12:40:52 +0100

    fix little bug

--- communityDiscordBot/Src/Interactions/Buttons/eventButton.js
+++ communityDiscordBot/Src/Interactions/Buttons/eventButton.js
@@ -1,18 +1,11 @@
-const config = require('../../Credentials/Config');
 const { conn } = require('../../functions/conn');
-const { log } = require('../../functions/log');
-const talkedRecently = new Set();
-
-const { inlineCode, ActionRowBuilder, ButtonBuilder, ButtonStyle, ChannelType, EmbedBuilder, PermissionsBitField } = require('discord.js');
 
 module.exports = {
 	name: 'event',
 	run: async (client, interaction) => {
 		const currentTicketId = await conn('SELECT MAX(id) FROM `events`');
 
-		let maxId = currentTicketId[0]['MAX(id)'];
-		let newId = maxId + 1;
-		await conn('INSERT INTO `events` (id, discordUserName, discordUserId) VALUES (?,?,?)', [newId, interaction.user.username, interaction.user.id]);
+		await conn('INSERT INTO `events` (discordUserName, discordUserId) VALUES (?,?)', [interaction.user.username, interaction.user.id]);
 		await interaction.reply({
 			content: `🎉 Du hast dich erfolgreich für das Gewinnspiel angemeldet. 🎉`,
 			ephemeral: true,
--- communityDiscordBot/Src/Interactions/SlashCommands/Guilds/1149013208959549551/User/event.js
+++ communityDiscordBot/Src/Interactions/SlashCommands/Guilds/1149013208959549551/User/event.js
@@ -16,7 +16,7 @@ module.exports = {
 				iconURL: `${interaction.guild.iconURL()}`,
 			})
 			.setDescription(
-				'Zur Feier von 1000 Discord Mitglieder wollen wir mit euch ein kleines Gewinnspiel machen. \n\nWir möchten unter euch 5x 200 Premium Punkte verlosen. Alles, was ihr für die Teilnahme tun müsst, ist auf Teilnehmen zu drücken. \n\nJeder User kann nur einmal teilnehmen! Die 5. Gewinner werden am 1. März 2024 per Zufallsprinzip ausgelost. \n\nViele Grüße\nEuer Stämme-Team',
+				'Zur Feier von 1000 Discord Mitglieder wollen wir mit euch ein kleines Gewinnspiel machen. \n\nWir möchten unter euch 5x 200 Premium Punkte verlosen. Alles, was ihr für die Teilnahme tun müsst, ist auf Teilnehmen zu drücken. \n\nJeder User kann nur einmal teilnehmen! Die Gewinner werden am 1. März 2024 per Zufallsprinzip ausgelost. \n\nViele Grüße\nEuer Stämme-Team',
 			)
 			.setTimestamp()
 			.setColor(0xed3d7d);
